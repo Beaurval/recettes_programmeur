0b220d95a60521b74cf983fe58a33660ffc739ea
FremachBeaury <beaury.valentin@gmail.com>
2019-02-06 01:36:08 +0100

    Création de la page index responsive

--- index.php
+++ index.php
@@ -1,8 +1,37 @@
-<?php
-
-/**
- * Created by PhpStorm.
- * User: Valentin Beaury
- * Date: 05/02/2019
- * Time: 15:39
- */
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php require_once 'templates/bootstrap.php';?>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="assets/logo.png">
+    <title>Recettes</title>
+</head>
+<body>
+<div class="container">
+    <?php
+    require_once 'templates/navbar.php';
+    ?>
+    <article class="recettes bg-custom">
+        <h2 class="text-custom">Nos recettes</h2>
+            <div class="row p-2">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+                <img class="col-xl-3 col-lg-4 col-sm-6 mb-2" src="assets/recette.jpg" alt="croissants">
+            </div>
+    </article>
+</div>
+</body>
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-custom border-bottom">
+    <div class="titre ">
+        <a class="navbar-brand" href="#">
+            <img src="assets/logo.png" width="100" height="80" class="d-inline-block align-top" alt="logo">
+        </a>
+        <label class="text-custom" href="#">Les recettes pour tous !</label>
+    </div>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="mx-auto col-xl-9 col-sm-4">
+        <form class="form-inline">
+            <input class="form-control rounded-pill col-12" type="search" placeholder="Chercher une recette" aria-label="Search">
+        </form>
+    </div>
+
+
+    <div class="login titre">
+        <button id ='login' type="button" class="btn btn-orange text-light rounded-pill">
+            <i class="fas fa-user"></i>
+        </button>
+        <label class="text-custom" for="login" >Connexion</label>
+    </div>
+
+</nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-custom">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link " href="#">Nos recettes<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Ajouter une recette</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Vos recettes</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
